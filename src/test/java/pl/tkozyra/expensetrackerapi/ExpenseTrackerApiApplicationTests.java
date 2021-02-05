8e9aa919428ad534d2847d0e433b641a0b9e1f98
tkozyra <tomekkozyra@gmail.com>
2021-02-05 22:13:59 +0100

    Initial commit

---
+++ src/test/java/pl/tkozyra/expensetrackerapi/ExpenseTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tkozyra.expensetrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
